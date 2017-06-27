94cb00713f302e1953088d2ffbbf41c084330084
mschaeffner <m@localhost>
2017-06-27 19:52:27 +1200

    create Heading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Heading from './Heading';
 
 class App extends Component {
 
   render() {
     return (
-      <div>app content here</div>
+      <div>
+        <Heading/>
+        app content here
+      </div>
     );
   }
 
---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Heading extends Component {
+
+  render() {
+    return (
+      <h1>Heading</h1>
+    );
+  }
+
+}
+
+export default Heading;
