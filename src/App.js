e7f30e0096fc5a53af08e77babc0a063e8a6d177
mschaeffner <m@localhost>
2017-06-27 21:15:35 +1200

    Show static list of users

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import Heading from './Heading';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        { name: { first: 'Alice', last: 'A' }, email: 'alice@example.com' },
+        { name: { first: 'Bob', last: 'B' }, email: 'bob@example.com' },
+        { name: { first: 'Charlie', last: 'C' }, email: 'charlie@example.com' }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
         <Heading/>
-        app content here
+        {this.state.users.map(user =>
+          <div>{user.name.first + ' ' + user.name.last + ' ' + user.email}</div>
+        )}
       </div>
     );
   }
